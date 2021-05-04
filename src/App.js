bf898ff3508479f61d2e0803b6e37b9f4c4f1932
MarieAnge5 <77550571+MarieAnge5@users.noreply.github.com>
2021-05-04 23:33:01 +0100

    last update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import logo from  "./logo.png"; 
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo 
-        img-fluid" alt="logo"/>
+        <img src={logo} className="App-logo img-fluid" alt="logo"/>
       </header>
     </div>
   );
 }
 
-export default App;
