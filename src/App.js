f603ec0a115c8b1790dcee64bc8efa31ab688b68
MarieAnge5 <77550571+MarieAnge5@users.noreply.github.com>
2021-05-05 22:23:36 +0100

    Add the search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import logo from  "./logo.png"; 
+import Dictionary from "./Dictionary"; 
 
 export default function App() {
   return (
     <div className="App">
+      <div className="Container">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo"/>
       </header>
+      <main>
+        <Dictionary/>
+      </main>
+      <footer className="App-footer">
+       <small> Coded by SheCodes </small>
+      </footer>
+      </div>
     </div>
   );
 }
